6534ef3ee51c86476fc7c3ec6cfe5002901725c0
samclarke <sam@samclarke.com>
2012-06-27 21:16:56 +0100

    Update build script

--- build/build.sh
+++ build/build.sh
@@ -1,54 +1,76 @@
 #! /bin/bash
 
-# To use this build file type /build/build.sh in terminal from the root directory.
+# To use this build file type
+# $ ./build/build.sh
+# in the terminal from the root directory.
+#
+# You must have uglifyjs, lessc, jsdoc, glue and yui-compressor
+# in order to run this buil script.
 
-# You must have uglifyjs, lessc and yui-compressor in order to run this script.
+USAGE="Usage: `basename $0` [--css|--js|--docs]"
 
-# To create the CSS sprites you will need "glue"
+DO_CSS=false
+DO_JS=true
+DO_DOCS=false
 
-# compress CSS
-echo "Creating CSS sprites"
-
-glue themes/icons/src/famfamfam themes/icons --less --algorithm=vertical --optipng --namespace=sceditor-button
-sed -i 's/famfamfam\-//' themes/icons/famfamfam.less
-sed -i 's/url,/link,/' themes/icons/famfamfam.less
-sed -i 's/url{/link{/' themes/icons/famfamfam.less
-sed -i 's/{/ div {/' themes/icons/famfamfam.less
-sed -i 's/,/ div,/' themes/icons/famfamfam.less
-sed -i 's/grip div/grip/' themes/icons/famfamfam.less
-
-for f in themes/icons/*.png
-do
-	echo "Processing $f file..";
-
-	filename=$(basename "$f")
-	filename="${filename%.*}"
-
-	cp $f minified/themes/$filename.png
+while true; do
+	case "$1" in
+		-c | --css )	DO_CSS=true; shift ;;
+		-j | --js )	DO_JS=true; shift ;;
+		-d | --docs )	DO_DOCS=true; shift ;;
+		* ) break ;;
+	esac
 done
 
 
-echo "Minimising CSS"
+if $DO_CSS; then
+	echo "Creating CSS sprites"
 
-for f in themes/*.less
-do
-	echo "Processing $f file..";
+	glue themes/icons/src/famfamfam themes/icons --less --algorithm=vertical --optipng --namespace=sceditor-button
+	sed -i 's/famfamfam\-//' themes/icons/famfamfam.less
+	sed -i 's/url,/link,/' themes/icons/famfamfam.less
+	sed -i 's/url{/link{/' themes/icons/famfamfam.less
+	sed -i 's/{/ div {/' themes/icons/famfamfam.less
+	sed -i 's/,/ div,/' themes/icons/famfamfam.less
+	sed -i 's/grip div/grip/' themes/icons/famfamfam.less
 
-	filename=$(basename "$f")
-	filename="${filename%.*}"
+	for f in themes/icons/*.png
+	do
+		echo "Processing $f file..";
 
-	lessc --yui-compress $f > minified/themes/$filename.min.css
-done
+		filename=$(basename "$f")
+		filename="${filename%.*}"
 
-yui-compressor --type css -o minified/jquery.sceditor.default.min.css jquery.sceditor.default.css
+		cp $f minified/themes/$filename.png
+	done
 
+	echo "Minimising CSS"
+	for f in themes/*.less
+	do
+		echo "Processing $f file..";
 
-# compres JS
-echo "Minimising JavaScript"
+		filename=$(basename "$f")
+		filename="${filename%.*}"
 
-cat jquery.sceditor.js jquery.sceditor.bbcode.js > minified/jquery.sceditor.min.js
+		lessc --yui-compress $f > minified/themes/$filename.min.css
+	done
 
-uglifyjs -nc --overwrite minified/jquery.sceditor.min.js
+	yui-compressor --type css -o minified/jquery.sceditor.default.min.css jquery.sceditor.default.css
+fi
 
-#java -jar build/compiler.jar --js=jquery.sceditor.js --js=jquery.sceditor.bbcode.js --js_output_file=minified/jquery.sceditor.min.js
+if $DO_JS; then
+	echo "Minimising JavaScript"
+
+	cat jquery.sceditor.js jquery.sceditor.bbcode.js > minified/jquery.sceditor.min.js
+
+	uglifyjs -nc --overwrite minified/jquery.sceditor.min.js
+
+	#java -jar build/compiler.jar --js=jquery.sceditor.js --js=jquery.sceditor.bbcode.js --js_output_file=minified/jquery.sceditor.min.js
+fi
+
+if $DO_DOCS; then
+	echo "Creating Docs"
+
+	jsdoc -d=./doc ./
+fi
 
