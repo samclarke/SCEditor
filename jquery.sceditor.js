b7d6dbeb407454503ba6773aa0872180a681369f
samclarke <sam@samclarke.com>
2012-07-12 22:06:32 +0100

    Fix whitespace removal bug
    Fix whitespace removal bug causing some whitespace to be left

--- jquery.sceditor.js
+++ jquery.sceditor.js
@@ -3035,6 +3035,16 @@
 			this.traverse(root, function(node) {
 				if(node.nodeType === 3 && $(node).parents('code, pre').length === 0 && node.nodeValue)
 				{
+					// new lines in text nodes are always ignored in normal handling
+					node.nodeValue = node.nodeValue.replace(/[\r\n]/, "");
+
+					//remove empty nodes
+					if(!node.nodeValue.length)
+					{
+						node.parentNode.removeChild(node);
+						return;
+					}
+
 					if(!/\S|\u00A0/.test(node.nodeValue))
 						node.nodeValue = " ";
 					else if(regex.test(node.nodeValue))
