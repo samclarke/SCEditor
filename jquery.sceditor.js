bf637d0bb6577d1cea698588ae578efa322f193c
samclarke <sam@samclarke.com>
2012-07-17 01:26:35 +0100

    Fix IE blur box showing

--- jquery.sceditor.js
+++ jquery.sceditor.js
@@ -1384,7 +1384,7 @@
 			// Must use an element that isn't display:hidden or visibility:hidden for iOS
 			// so create a special blur element to use
 			if(!$blurElm)
-				$blurElm = $('<input style="width:0; height:0; opacity:0" type="text" />').appendTo($editorContainer);
+				$blurElm = $('<input style="width:0; height:0; opacity:0; filter: alpha(opacity=0)" type="text" />').appendTo($editorContainer);
 
 			$blurElm.removeAttr("disabled")
 				.focus()
