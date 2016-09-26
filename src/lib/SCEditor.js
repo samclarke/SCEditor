67f7ec2b79425218f90a76088184d15100ddf307
webevolutis <webmaster@tonguide.com>
2016-09-26 17:39:30 +0200

    Update SCEditor.js

--- src/lib/SCEditor.js
+++ src/lib/SCEditor.js
@@ -3354,7 +3354,7 @@
 				character = SPECIAL_KEYS[which] ||
 					String.fromCharCode(which).toLowerCase();
 
-			if (e.ctrlKey||e.metaKey) {
+			if (e.ctrlKey || e.metaKey) {
 				shortcut.push('ctrl');
 			}
 
