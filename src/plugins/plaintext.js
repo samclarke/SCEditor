ec0cb7637933465207ccc41f695d6807392d92dc
Sam Clarke <sam@samclarke.com>
2016-12-07 18:15:54 +0000

    Add plaintext plugin. Fixes #32, fixes #511, fixes #334

---
+++ src/plugins/plaintext.js
@@ -0,0 +1,56 @@
+/**
+ * SCEditor Plain Text Plugin
+ * http://www.sceditor.com/
+ *
+ * Copyright (C) 2016, Sam Clarke (samclarke.com)
+ *
+ * SCEditor is licensed under the MIT license:
+ *	http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author Sam Clarke
+ * @requires jQuery
+ */
+(function ($) {
+	'use strict';
+
+	/**
+	 * Options:
+	 *
+	 * pastetext.addButton - If to replace the plaintext button with a toggle
+	 *                       button that enables and disables plain text mode.
+	 *
+	 * pastetext.enabled - If the plain text button should be enabled at start
+	 *                     up. Only applies if addButton is enabled.
+	 */
+	$.sceditor.plugins.plaintext = function () {
+		var plainTextEnabled = true;
+
+		this.init = function () {
+			var commands = this.commands;
+			var opts = this.opts;
+
+			if (opts && opts.plaintext && opts.plaintext.addButton) {
+				plainTextEnabled = opts.plaintext.enabled;
+
+				commands.pastetext = $.extend(commands.pastetext || {}, {
+					state: function () {
+						return plainTextEnabled ? 1 : 0;
+					},
+					exec: function () {
+						plainTextEnabled = !plainTextEnabled;
+					}
+				});
+			}
+		};
+
+		this.signalPasteRaw = function (data) {
+			if (plainTextEnabled) {
+				if (data.html && !data.text) {
+					data.text = $('<div />').html(data.html)[0].innerText;
+				}
+
+				data.html = null;
+			}
+		};
+	};
+})(jQuery);
