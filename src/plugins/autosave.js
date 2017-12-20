1837c6d254d7b1f9abd33ad4b7bbe2d13229bad6
Sam Clarke <sam@samclarke.com>
2017-12-20 14:35:02 +0000

    Add copyright header to autosave plugin

--- src/plugins/autosave.js
+++ src/plugins/autosave.js
@@ -1,3 +1,14 @@
+/**
+ * SCEditor AutoSave Plugin
+ * http://www.sceditor.com/
+ *
+ * Copyright (C) 2017, Sam Clarke (samclarke.com)
+ *
+ * SCEditor is licensed under the MIT license:
+ *	http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author Sam Clarke
+ */
 (function (sceditor) {
 	'use strict';
 
