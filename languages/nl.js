0963a9dcb4ab9f6f7f92407d971b3e5ea1c1d7ef
NekoJonez <NekoJonez@users.noreply.github.com>
2022-08-21 17:49:36 +0200

    Update Dutch translation and fixes (#897)
    Updated the Dutch translation and fixes a few translations.

--- languages/nl.js
+++ languages/nl.js
@@ -1,10 +1,15 @@
+/**
+ * @author Pieterjan Deneys (NekoJonez) - https://arpegi.wordpress.com
+ * @license [MIT](http://www.opensource.org/licenses/mit-license.php)
+ */
+
 (function () {
 	'use strict';
 
 	sceditor.locale['nl'] = {
 		'Bold': 'Vet',
-		'Italic': 'Schuingedrukt',
-		'Underline': 'Onderstreept',
+		'Italic': 'Cursief',
+		'Underline': 'Onderlijnd',
 		'Strikethrough': 'Doorhalen',
 		'Subscript': 'Subscript',
 		'Superscript': 'Superscript',
@@ -12,17 +17,17 @@
 		'Center': 'Centreren',
 		'Align right': 'Rechts uitlijnen',
 		'Justify': 'Uitvullen',
-		'Font Name': 'Fontnaam',
-		'Font Size': 'Fontgrootte',
-		'Font Color': 'Fontkleur',
+		'Font Name': 'Lettertype naam',
+		'Font Size': 'Lettergrootte',
+		'Font Color': 'Lettertype kleur',
 		'Remove Formatting': 'Verwijder opmaak',
 		'Cut': 'Knippen',
-		'Your browser does not allow the cut command. Please use the keyboard shortcut Ctrl/Cmd-X': 'Je browser staat het knippen commando niet toe. Gebruik de toetsenbord sneltoets Ctrl / Cmd-X',
+		'Your browser does not allow the cut command. Please use the keyboard shortcut Ctrl/Cmd-X': 'Je browser staat het knippen commando niet toe. Gebruik de toetsenbord sneltoets Ctrl/Cmd-X',
 		'Copy': 'Kopiëren',
-		'Your browser does not allow the copy command. Please use the keyboard shortcut Ctrl/Cmd-C': 'Je browser staat het kopieer commando niet toe. Gebruik de toetsenbord sneltoets Ctrl / Cmd-C',
+		'Your browser does not allow the copy command. Please use the keyboard shortcut Ctrl/Cmd-C': 'Je browser staat het kopieer commando niet toe. Gebruik de toetsenbord sneltoets Ctrl/Cmd-C',
 		'Paste': 'Plakken',
-		'Your browser does not allow the paste command. Please use the keyboard shortcut Ctrl/Cmd-V': 'Je browser staat het plakken commando niet toe. Gebruik de toetsenbord sneltoets  Ctrl / Cmd-V',
-		'Paste your text inside the following box:': 'Plak je tekst in de volgende locatie:',
+		'Your browser does not allow the paste command. Please use the keyboard shortcut Ctrl/Cmd-V': 'Je browser staat het plakken commando niet toe. Gebruik de toetsenbord sneltoets Ctrl/Cmd-V',
+		'Paste your text inside the following box:': 'Plak je tekst in het volgende vak:',
 		'Paste Text': 'Tekst plakken',
 		'Bullet list': 'Opsomming',
 		'Numbered list': 'Genummerde lijst',
@@ -31,27 +36,37 @@
 		'Rows:': 'Rijen',
 		'Cols:': 'Kolommen',
 		'Insert a table': 'Tabel',
-		'Insert a horizontal rule': 'Horizontale regel',
+		'Insert a horizontal rule': 'Horizontale regel invoegen',
 		'Code': 'Code',
-		'Insert a Quote': 'Citeren',
 		'Width (optional):': 'Breedte (optioneel):',
 		'Height (optional):': 'Hoogte (optioneel):',
-		'Insert an image': 'Afbeelding',
+		'Insert an image': 'Afbeelding invoegen',
 		'E-mail:': 'E-mail',
-		'Insert an email': 'E-mail',
+		'Insert an email': 'E-mail invoegen',
 		'URL:': 'URL:',
-		'Insert a link': 'Link',
+		'Insert a link': 'Link invoegen',
 		'Unlink': 'Link verwijderen',
 		'More': 'Meer',
-		'Insert an emoticon': 'Emoticon',
+		'Insert an emoticon': 'Emoticon invoegen',
 		'Video URL:': 'Video URL',
 		'Insert': 'Invoegen',
-		'Insert a YouTube video': 'YouTube-video',
-		'Insert current date': 'Huidige datum',
-		'Insert current time': 'Huidige tijd',
+		'Insert a YouTube video': 'YouTube video invoegen',
+		'Insert current date': 'Huidige datum invoegen',
+		'Insert current time': 'Huidige tijd invoegen',
 		'Print': 'Print',
 		'View source': 'Bron bekijken',
+		'Description (optional):': 'Beschrijving (optioneel):',
+		'Enter the image URL:': 'Voer de afbeelding URL in:',
+		'Enter the e-mail address:': 'Voer het e-mailadres in:',
+		'Enter the displayed text:': 'Voer de getoonde tekst in:',
+		'Enter URL:': 'Voer de URL in:',
+		'Enter the YouTube video URL or ID:': 'Voer de YouTube video URL of ID in:',
+		'Insert a Quote': 'Citaat toevoegen',
+		'Invalid YouTube video': 'Ongeldige YouTube video',
+		'Drop files here': 'Plaats bestanden hier',
 
-		dateFormat: 'day.month.year'
+		// month format, replace - with the date format separator and order in the
+		// order used
+		dateFormat: 'day-month-year'
 	};
 })();
