3161e47265fd66b8375187fd5a5adf527a672e70
Sam <sam@samclarke.com>
2024-03-31 21:42:15 +0100

    Fix formatting so passes tests
    Changes spaces to tabs, adds a few spaces before opening brackets.
    
    Also adds a comment to tell ESLint `EmojiMart` is a global variable.

--- src/plugins/emojis.js
+++ src/plugins/emojis.js
@@ -10,77 +10,77 @@
  * @fileoverview SCEditor Paragraph Formatting Plugin
  * @author Sam Clarke
  */
+/*global EmojiMart*/
+(function (sceditor) {
+	'use strict';
 
-(function(sceditor) {
-    'use strict';
+	sceditor.plugins.emojis = function () {
+		const base = this;
 
-    sceditor.plugins.emojis = function() {
-        const base = this;
+		/**
+		 * Function for the exec and txtExec properties
+		 *
+		 * @param  {node} caller
+		 * @private
+		 */
+		var emojisCmd = function (caller) {
+			const editor = this,
+				content = document.createElement('div'),
+				emojis = editor.opts.emojis || [],
+				perLine = Math.sqrt(Object.keys(emojis).length);
 
-        /**
-         * Function for the exec and txtExec properties
-         *
-         * @param  {node} caller
-         * @private
-         */
-        var emojisCmd = function(caller) {
-            const editor = this,
-                content = document.createElement('div'),
-                emojis = editor.opts.emojis || [],
-                perLine = Math.sqrt(Object.keys(emojis).length);
+			if (!emojis.length) {
+				const pickerOptions = { onEmojiSelect: handleSelect };
+				const picker = new EmojiMart.Picker(pickerOptions);
 
-            if (!emojis.length) {
-                const pickerOptions = { onEmojiSelect: handleSelect };
-                const picker = new EmojiMart.Picker(pickerOptions);
+				content.appendChild(picker);
+			} else {
+				var line = document.createElement('div');
 
-                content.appendChild(picker);
-            } else {
-                var line = document.createElement('div');
+				sceditor.utils.each(emojis,
+					function (_, emoji) {
+						const emojiElem = document.createElement('span');
 
-                sceditor.utils.each(emojis,
-                    function (_, emoji) {
-                        const emojiElem = document.createElement('span');
+						emojiElem.className = 'sceditor-option';
+						emojiElem.style = 'cursor:pointer';
 
-                        emojiElem.className = 'sceditor-option';
-                        emojiElem.style = 'cursor:pointer';
+						emojiElem.appendChild(document.createTextNode(emoji));
 
-                        emojiElem.appendChild(document.createTextNode(emoji));
+						emojiElem.addEventListener('click',
+							function (e) {
+								editor.closeDropDown(true);
 
-                        emojiElem.addEventListener('click',
-                            function (e) {
-                                editor.closeDropDown(true);
+								editor.insert(e.target.innerHTML);
 
-                                editor.insert(e.target.innerHTML);
+								e.preventDefault();
+							});
 
-                                e.preventDefault();
-                            });
+						if (line.children.length >= perLine) {
+							line = document.createElement('div');
+						}
 
-                        if (line.children.length >= perLine) {
-                            line = document.createElement('div');
-                        }
+						content.appendChild(line);
 
-                        content.appendChild(line);
+						line.appendChild(emojiElem);
+					});
+			}
 
-                        line.appendChild(emojiElem);
-                    });
-            }
+			editor.createDropDown(caller, 'emojis', content);
 
-            editor.createDropDown(caller, 'emojis', content);
+			function handleSelect(emoji) {
+				editor.insert(emoji.native);
 
-            function handleSelect(emoji) {
-                editor.insert(emoji.native);
+				editor.closeDropDown(true);
+			}
+		};
 
-                editor.closeDropDown(true);
-            }
-        };
-
-        base.init = function () {
-            this.commands.emojis = {
-                exec: emojisCmd,
-                txtExec: emojisCmd,
-                tooltip: 'Insert emoji',
-                shortcut: 'Ctrl+E'
-            };
-        };
-    };
+		base.init = function () {
+			this.commands.emojis = {
+				exec: emojisCmd,
+				txtExec: emojisCmd,
+				tooltip: 'Insert emoji',
+				shortcut: 'Ctrl+E'
+			};
+		};
+	};
 })(sceditor);
